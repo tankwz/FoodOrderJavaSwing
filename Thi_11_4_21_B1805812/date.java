f753db61b6b7e8fceb5c8bf7b7d75eed887dc484
tan <102865182+tankwz@users.noreply.github.com>
2023-11-14 16:43:30 +0700

    Add files via upload

---
+++ Thi_11_4_21_B1805812/date.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package B1805812;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class date implements Serializable {
+    private Date day;
+    public Date getday() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+}
+
